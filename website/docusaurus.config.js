0fca9ea3c56e89195b78aea9e7c0102459a2df90
PangTeen <1687289734@qq.com>
2026-07-18 13:41:15 +0800

    fix: 这次一定成功！

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://hungteen.github.io/HTLib/',
+  url: 'https://hungteen.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/HTLib',
