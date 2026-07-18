7ce531f1f5d07da915f44ea2ae7bd8247e5df434
PangTeen <1687289734@qq.com>
2026-07-18 13:37:13 +0800

    fix: 改变网站根目录

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://hungteen.github.io/HTLib/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/HTLib',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
