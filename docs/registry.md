50b51697db5c07e29a8b6638e21914e040bfe920
PangTeen <1687289734@qq.com>
2026-07-25 16:47:59 +0800

    feat: 完善博客部分

--- docs/registry.md
+++
@@ -1,7 +0,0 @@
-## SimpleRegistry
-### SimpleEntry
-所有注册项的顶层接口：[SimpleEntry](../common/src/main/java/hungteen/htlib/api/registry/SimpleEntry.java)
-
-
-## CodecRegistry
-
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/HungTeen/HTLib/tree/master/website',
         },
         blog: {
           showReadingTime: true,
@@ -75,7 +75,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/HungTeen/HTLib/tree/master/website',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
